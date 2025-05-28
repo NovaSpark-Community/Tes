984741ee128cc4939563ef7f2dcce4c862085d5a
NovaSpark-Community <team.sunflare@gmail.com>
2025-05-28 00:41:26 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -66,7 +66,7 @@ global.insta = 'https://www.instagram.com/dev.criss_vx'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-//FAST MAURO DUERME
+global.catalogo = fs.readFileSync('./media/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'SUNFLARE 乂 TEAM', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.multiplier = 70
 
